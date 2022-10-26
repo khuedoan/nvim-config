2382d483f46bcede7c6153805f81009b08345751
Khue Doan <mail@khuedoan.com>
2022-10-26 14:59:07 +0700

    chore: update from dotfiles

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -7,16 +7,29 @@ local actions = require("telescope.actions")
 
 telescope.setup({
     defaults = {
-        path_display = { "smart" },
+        path_display = { "absolute" },
         file_ignore_patterns = { ".git/", "node_modules" },
 
         mappings = {
             i = {
                 ["<Down>"] = actions.cycle_history_next,
                 ["<Up>"] = actions.cycle_history_prev,
+                ["<C-n>"] = actions.cycle_history_next,
+                ["<C-p>"] = actions.cycle_history_prev,
                 ["<C-j>"] = actions.move_selection_next,
                 ["<C-k>"] = actions.move_selection_previous,
             },
         },
     },
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+        },
+    },
 })
+
+telescope.load_extension("fzf")
